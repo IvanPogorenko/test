21be30c18466ed1e81b8d6da96c82d0e18dcf123
ivan_pogorenko <ipogorenko@yandex.ru>
2025-10-28 20:51:51 +0300

    first commit

---
+++ src/components/sort-panel/sort-panel.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {SortPanelProps} from "../../interfaces/car-sort.interface";
+
+const SertPanel: React.FC<SortPanelProps> = ({sortBy, onSortChange}) => {
+    return (
+        
+    );
+};
